f5da09e76256418e8da563cda7618b6e81b222db
T.J. Zientek <37338907+tjzientek@users.noreply.github.com>
2018-06-27 22:33:34 -0400

    Homework 7

---
+++ Homework-07/config.py
@@ -0,0 +1,6 @@
+# T.J.'s Keys
+
+consumer_key = "uP7fG0F9gujxkZnpi6AULerPn"
+consumer_secret = "SuzWmhTM80e1c1Hd36uhRhYYhiQnMXSoeBasNL3tb0lP6HEqwE"
+access_token = "125738631-eaOBnbwjIff7iAfAvXaVxwrdCFZGMk7qJlmsfBdd"
+access_token_secret = "e288JTGKVK2o5Dymu8pB7qg2LKCtbDrZB4BsSAffqTrlp"
