e773a0cdf042be298d3bf2504aa7bba5eb57e90b
T.J. Zientek <37338907+tjzientek@users.noreply.github.com>
2018-07-28 21:29:02 -0400

    Homwork 9

--- Homework-09/climate_api.py
+++ Homework-09/climate_api.py
@@ -39,7 +39,7 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return (
-        f"Hawaii's Precipitation and Temperature Date<br/>"
+        f"<h1>Hawaii's Precipitation and Temperature Date</h1><br/>"
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
@@ -58,10 +58,10 @@ def precipitation():
     date_12 = dt.datetime.strptime(max_date, '%Y-%m-%d') - dt.timedelta(days=365)
     min_date = date_12.strftime('%Y-%m-%d')
 
-    tobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= min_date).filter(Measurement.date <= max_date).all()
-    dict_tobs = dict(tobs)
-    if len(tobs) > 0:
-        return jsonify(dict_tobs)
+    prcp = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= min_date).filter(Measurement.date <= max_date).all()
+    dict_prcp = dict(prcp)
+    if len(dict_prcp) > 0:
+        return jsonify(dict_prcp)
 
     return jsonify({"error": f"No precipitation data found."}), 404
 
@@ -69,13 +69,45 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
 
-    stations = session.query(Station).all()
-    dict_stations = dict(stations)
-    if (len(dict_stations) > 0):
-        return jsonify(dict_stations)
+    stations = session.query(Station.id, Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
+    #dict_stations = dict(stations)
+    if (len(stations) > 0):
+        return jsonify(stations)
 
     return jsonify({"error": f"No station data found."}), 404
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+    
+    max_date1 = session.query(func.max(Measurement.date)).filter(Measurement.station=="USC00519281").\
+    group_by(Measurement.station).all()
+    max_date = max_date1[0][0]
+
+    date_12 = dt.datetime.strptime(max_date, '%Y-%m-%d') - dt.timedelta(days=365)
+    min_date = date_12.strftime('%Y-%m-%d')
+
+    tobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= min_date).filter(Measurement.date <= max_date).all()
+    dict_tobs = dict(tobs)
+    if len(dict_tobs) > 0:
+        return jsonify(dict_tobs)
+
+    return jsonify({"error": f"No temperature observations data found."}), 404
+
+@app.route("/api/v1.0/<startdate>")
+def temps1(startdate):
+
+    temps1 = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= startdate).all()
+
+    return jsonify(temps1)
+
+@app.route("/api/v1.0/<startdate>/<enddate>")
+def temps2(startdate, enddate):
+
+    temps2 = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= startdate).filter(Measurement.date <= enddate).all()
+
+    return jsonify(temps2)
 
 
 if __name__ == "__main__":
