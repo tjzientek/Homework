bda30a880fc7beefed8e94d2ceab684b3a9d3e20
T.J. Zientek <37338907+tjzientek@users.noreply.github.com>
2018-08-22 12:24:16 -0400

    Homework 13

--- Homework-13/Belly_Button/app.py
+++ Homework-13/Belly_Button/app.py
@@ -97,5 +97,22 @@ def samples(sample):
     return jsonify(data)
 
 
+@app.route("/wfreq/<sample>")
+def wfreq(sample):
+    sel = [
+        Samples_Metadata.sample,
+        Samples_Metadata.WFREQ,
+    ]
+
+    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
+
+    sample_wfreq = {}
+    for result in results:
+        sample_wfreq["sample"] = result[0]
+        sample_wfreq["WFREQ"] = result[1]
+
+    return jsonify(sample_wfreq)
+
+
 if __name__ == "__main__":
     app.run()
